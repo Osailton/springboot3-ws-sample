e8695383f21b8928c38d8b58f239f2acd281e711
Osailton <osailtonvieira@gmail.com>
2023-05-27 14:10:58 -0300

    Creating project

---
+++ src/main/java/com/sample/spring_ws_sample/SpringWsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring_ws_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsSampleApplication.class, args);
+	}
+
+}
